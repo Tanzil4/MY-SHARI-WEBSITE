97c092ff22b5dc53026fb4fc1fb04ee2eaf4b1c6
TasmiyaKhan8028 <155808841+TasmiyaKhan8028@users.noreply.github.com>
2024-05-12 23:43:38 -0700

    add file

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let meh1 = document.getElementById("myh1")
+let btn = document.getElementById("box")
+    
+    let fullname =   prompt("Enter your user name")
+    
+    
+    meh1.innerHTML = (fullname + " hellow welcome my  shayari app").toLocaleUpperCase()
